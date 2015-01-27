d98cd959bcc9f16a1a68e0293cc49484a83f1f41
StyroCupMan <styrocupman@gmail.com>
2015-01-27 14:16:47 -0700

    Solution

---
+++ AaronLambert/STGCodeChallenge150127/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace STGCodeChallenge150127
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(13, 34);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(257, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(104, 62);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 27);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 101);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "STG Code Challenge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ AaronLambert/STGCodeChallenge150127/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace STGCodeChallenge150127
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            DoTheWork();
+        }
+
+        private void DoTheWork()
+        {
+            string CCNbr = txtInput.Text.Trim();
+            ValidateCCNbr(CCNbr);
+        }
+
+        private void ValidateCCNbr(string CCNbr)
+        {
+            string OrigCCNbr = CCNbr;
+            string CardType = "";
+            int CCLen;
+
+            if (string.IsNullOrWhiteSpace(CCNbr))
+            {
+                InvalidCCNbr(OrigCCNbr);
+                return;
+            }
+
+            // Remove any dashes and spaces
+            CCNbr = CCNbr.Replace("-", "");
+            CCNbr = CCNbr.Replace(" ", "");
+            CCLen = CCNbr.Length;
+
+            // Determine Card Type and validate length
+            if (CCNbr.Substring(0, 1) == "4")
+            {
+                CardType = "Visa";
+                if (CCLen != 13 && CCLen != 16)
+                {
+                    InvalidCCNbr(OrigCCNbr, CardType);
+                    return;
+                }
+            }
+            else
+            {
+                int prefix = 0;
+                if (int.TryParse(CCNbr.Substring(0, 2), out prefix))
+                {
+                    if (prefix >= 51 && prefix <= 55)
+                    {
+                        CardType = "Master Card";
+                        if (CCLen != 16)
+                        {
+                            InvalidCCNbr(OrigCCNbr, CardType);
+                            return;
+                        }
+                    }
+                    else if (prefix == 37)
+                    {
+                        CardType = "American Express";
+                        if (CCLen != 15)
+                        {
+                            InvalidCCNbr(OrigCCNbr, CardType);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        InvalidCCNbr(OrigCCNbr);
+                        return;
+                    }
+                }
+                else
+                {
+                    InvalidCCNbr(OrigCCNbr);
+                    return;
+                }
+
+            }
+
+            // Validate the CC Nbr using the mathematical checksum
+            int checksum = 0;
+            if (!CalculateChecksum(CCNbr, out checksum))
+            {
+                InvalidCCNbr(OrigCCNbr, CardType);
+                return;
+            }
+
+            // The calculated value must be evenly divisible by 10
+            ShowResults(OrigCCNbr, CardType, (checksum % 10) == 0);
+        }
+
+        private void InvalidCCNbr(string CCNbr, string CardType = "")
+        {
+            ShowResults(CCNbr, CardType, false);
+        }
+
+        private void ValidCCNbr(string CCNbr, string CardType)
+        {
+            ShowResults(CCNbr, CardType, true);
+        }
+
+        private void ShowResults(string CCNbr, string CardType, bool Valid)
+        {
+            MessageBox.Show(string.Format("'{0}' is {1}a valid {2}credit card number.", CCNbr, (Valid ? "" : "NOT "), (string.IsNullOrEmpty(CardType) ? "" : CardType + " ")));
+        }
+
+        private bool CalculateChecksum(string CCNbr, out int checksum)
+        {
+            // Validate the CC Nbr using the mathematical checksum
+            checksum = 0;
+
+            // Must be a valid number - make sure each char is a digit
+            if (!CCNbr.All(Char.IsDigit))
+                return false;
+
+            // Reverse the CC Nbr so the checksum is easier to calculate
+            string RevCCNbr = string.Concat(Enumerable.Reverse(CCNbr));
+
+            for (int i = 0; i < RevCCNbr.Length; i++)
+            {
+                string c = RevCCNbr.Substring(i, 1);
+                int d = 0;
+                if (!int.TryParse(c, out d))
+                    return false;
+                
+                // We double the value of the alternate digits starting with the second one
+                if (i % 2 == 1)
+                {
+                    d *= 2;
+
+                    // If the value is now >= 10, add the digits separately
+                    if (d >= 10)
+                        d -= 9;
+                }
+
+                checksum += d;
+            }
+            return true;
+        }
+    }
+}
